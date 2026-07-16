3104021e3715f4bf63dcb50680445250423d9b4e
todaraiya <raiya.toda@ysl.co.jp>
2026-07-16 17:42:31 +0900

    AdminAction 作成中

--- source/src/main/java/action/AdminAction.java
+++ source/src/main/java/action/AdminAction.java
@@ -1,5 +1,43 @@
 package action;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import dto.UserDTO;
+import service.UserService;
+
 public class AdminAction {
+
+	HttpServletRequest request;
+
+	//コンストラクタ
+	public AdminAction(HttpServletRequest request) {
+		//自分のリクエスト部分にサーブレットからもらったリクエストを入れる。
+		this.request = request;
+	}
+
+	//ユーザ一覧メソッド
+	public String selectUser() throws UnsupportedEncodingException {
+		String page = "/WEB-INF/jsp/admin.jsp";
+
+		UserService service = new UserService();
+		ArrayList<UserDTO> userList = service.selectUsers();
+		request.setAttribute("userList", userList);
+
+		return page;
+	}
+
+	//ユーザ追加メソッド
+	public String registUser() throws UnsupportedEncodingException {
+		String page = "/WEB-INF/jsp/admin.jsp";
+		
+		
+
+		return page;
+	}
+
+	//ユーザ編集メソッド
 
 }

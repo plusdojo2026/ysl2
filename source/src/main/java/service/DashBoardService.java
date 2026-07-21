55f84d1a934802efaf55f4ca0ecd8423d6b4be65
mukogawa-ryo <ryo.mukogawa@ysl.co.jp>
2026-07-21 17:30:41 +0900

    ダッシュボードService作成中

--- source/src/main/java/service/DashBoardService.java
+++ source/src/main/java/service/DashBoardService.java
@@ -1,5 +1,53 @@
 package service;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import dao.TaskDAO;
+import dto.AllDTO;
+import dto.TaskDTO;
+
 public class DashBoardService {
+	
+	//コンストラクタ
+		public DashBoardService() {
+			super.access();
+		}
+	
+	//期限超過タスク検索（ダッシュボード）------------------------
+	public ArrayList<AllDTO> selectOverTasks(int userId){
+		ArrayList<AllDTO> taskList = new ArrayList<AllDTO>();
+		
+		TaskDAO dao = new TaskDAO(super.conn);
+		try {
+			taskList = dao.selectOverTasks(userId);
+		} catch (SQLException e) {
+			System.out.println("SQL文おかしい");
+			e.printStackTrace();
+		}
+		return taskList;
+	}
+
+	//担当タスク検索（ダッシュボード）-----------------------------
+	public ArrayList<AllDTO> selectAssignedTasks(int userId) {
+		ArrayList<AllDTO> taskList = new ArrayList<AllDTO>();
+		
+		TaskDAO dao = new TaskDAO(super.conn);
+		try {
+			taskList = dao.selectAssignedTasks(userId);
+		} catch (SQLException e) {
+			System.out.println("SQL文おかしい");
+			e.printStackTrace();
+		}
+		return taskList;
+	}
+	
+	
+	//進行中案件検索(ダッシュボード)--------------------------------------------
+		public ArrayList<TaskDTO> selectWorkingCases(){
+			ArrayList<TaskDTO> taskList = null;
+			
+			return taskList;
+		}
 
 }

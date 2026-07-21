eb9b1c9f43212f47ff2f658e18fe525570e70d10
TenyeeWang <tianyi.wang@ysl.co.jp>
2026-07-21 14:32:48 +0900

    userservice

--- source/src/main/java/service/UserService.java
+++ source/src/main/java/service/UserService.java
@@ -1,5 +1,49 @@
 package service;
 
-public class UserService {
+import java.util.ArrayList;
 
+import dto.UserDTO; 
+
+public class UserService extends DBAccess{
+	
+	
+	//DBアクセス
+	public UserService() {
+		super.access();
+	}
+	
+	//ユーザー登録メソッド
+	public int UserRegist(UserDTO Dto) {
+		int result = 0;
+		
+		return result;
+	}
+	
+	//ログインメソッド
+	public UserDTO login(String loginId, String pw) {
+		UserDTO user = null;
+		
+		return user;
+	}
+	
+	//パスワード変更・メンバー編集メソッド
+	public int UpdataUser(String loginId,String pw) {
+		int result = 0;
+		
+		return result;
+	}
+	
+	//ユーザー検査メソッド
+	public ArrayList<UserDTO>selectUsers(UserDTO dto){
+		ArrayList<UserDTO> userList = new ArrayList<>();
+		
+		return userList;
+	}
+	
+	//id重複チェックメソッド
+	public int checkDuplicateLoginId(String loginId) {
+		int result = 0;
+		
+		return result;
+	}
 }

2ea8566aead487b96fc7115f1cbfaa173df9d221
misaki-toraiwa <misaki.toraiwa@ysl.co.jp>
2026-07-22 17:16:41 +0900

    いろいろ

--- source/src/main/java/dao/ManHourDAO.java
+++ source/src/main/java/dao/ManHourDAO.java
@@ -1,7 +1,6 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -19,7 +18,7 @@ public class ManHourDAO {
 	}
 	
 	//工数登録
-	public int registManHour(Double today_man_hours, String work_details, Date work_date) throws SQLException{
+	public int registManHour(Double today_man_hours, String work_details, String work_date) throws SQLException{
 		int ans = 0;
 		String sql ="INSERT INTO man_hours (today_man_hours, work_details, work_date) VALUES(?,?,?)";
 		System.out.println(sql);
@@ -28,9 +27,13 @@ public class ManHourDAO {
 		pStmt.executeQuery();
 		pStmt.setDouble(1, today_man_hours);
 		pStmt.setString(2, work_details);
-		pStmt.setDate(3, work_date);
+		pStmt.setString(3, work_date);
 		pStmt.executeUpdate();
+		
+		pStmt.close();
+
 		return ans;
+
 	}
 
 	//削除
@@ -41,18 +44,19 @@ public class ManHourDAO {
 		PreparedStatement pStmt = conn.prepareStatement(sql);
 		pStmt.setInt(1, man_hours_id);
 		pStmt.executeUpdate();
-		
+
+		pStmt.close();
+
 		return ans;
 	}
 
 	
 	//案件名・タスク名（工数登録）
-	public ArrayList<AllDTO> selectCaseName(int task_id) throws SQLException {
+	public ArrayList<AllDTO> selectCaseName() throws SQLException {
 		ArrayList<AllDTO> list = new ArrayList<AllDTO>();
 		String sql ="SELECT case_name, task_name FROM tasks t JOIN cases c ON t.case_id = c.case_id WHERE t.task_id = ?";
 		System.out.println(sql);
 		PreparedStatement pStmt = conn.prepareStatement(sql);
-		pStmt.setInt(1, task_id);
 		
 		//SELECT文を実行し結果票を取得
 		ResultSet rs = pStmt.executeQuery();
@@ -60,42 +64,51 @@ public class ManHourDAO {
 		//移し替え
 		while(rs.next()) {
 			AllDTO dto = new AllDTO();
-			dto.setTaskId(rs.getInt("task_id"));
+			dto.setCaseName(rs.getString("case_name"));
+			dto.setTaskName(rs.getString("task_name"));
 			list.add(dto);
 		}
+		rs.close();
+		pStmt.close();
+
 		return list;
 	}
 
 
 	//工数ログ(タスク詳細)
-	public ArrayList<AllDTO> sumCaseManHours(int task_id) throws SQLException {
+	public ArrayList<AllDTO> sumCaseManHours() throws SQLException {
 		ArrayList<AllDTO> list = new ArrayList<AllDTO>();
 		String sql ="SELECT m.work_date AS '作業日', t.manager AS '担当者', m.today_man_hours AS '工数', m.work_details AS '作業内容' FROM man_hours m"
-					+ "JOIN tasks t ON m.task_id = t.task_id WHERE m.task_id LIMIT 4";
+					+ "JOIN tasks t ON m.task_id = t.task_id ORDER BY m.work_date";
 		System.out.println(sql);
 		PreparedStatement pStmt = conn.prepareStatement(sql);
-		pStmt.setInt(1,task_id);
 		
 		//セレクト文を実行し結果票を取得
 		ResultSet rs = pStmt.executeQuery();
 		
 		while(rs.next()) {
 			AllDTO dto = new AllDTO();
-			dto.setTaskId(rs.getInt("task_id"));
+			dto.setWorkDate(rs.getString("work_date"));
+			dto.setTaskName(rs.getString("task_name"));
+			dto.setManager(rs.getInt("manager"));
+			dto.setTodayManHours(rs.getDouble("today_man_hours"));
+			dto.setWorkDetails(rs.getString("work_details"));
 			list.add(dto);
 		}
+		rs.close();
+		pStmt.close();
+
 		return list;
 		
 	}
 
 	//実績工数
-	public ArrayList<AllDTO> selectManHours(String case_id) throws SQLException {
+	public ArrayList<AllDTO> selectManHours() throws SQLException {
 		ArrayList<AllDTO> list = new ArrayList<AllDTO>();
 		String sql ="SELECT t.case_id AS '案件コード', c.case_name AS '案件名', SUM(today_man_hours) AS '実績工数'"
 					+"FROM tasks t JOIN man_hours m ON t.task_id = m.task_id JOIN cases c ON t.case_id = c.case_id GROUP BY t.case_id";
 		System.out.println(sql);
 		PreparedStatement pStmt = conn.prepareStatement(sql);
-		pStmt.setString(1, case_id);
 		
 		//SELECT文を実行し結果票を取得
 		ResultSet rs = pStmt.executeQuery();
@@ -104,12 +117,43 @@ public class ManHourDAO {
 		while(rs.next()) {
 			AllDTO dto = new AllDTO();
 			dto.setCaseId(rs.getString("case_id"));
+			dto.setCaseName(rs.getString("case_name"));
+			dto.setTodayManHours(rs.getDouble("today_man_hours"));
 			list.add(dto);
 		}
+		rs.close();
+		pStmt.close();
+
 		return list;
 		
 	}
 
-	
+	//タスクごとの実績工数
+		public ArrayList<AllDTO> sumTaskManHours() throws SQLException {
+			ArrayList<AllDTO> list = new ArrayList<AllDTO>();
+			String sql ="SELECT tasks.task_id, task_name , SUM(today_man_hours) AS '実績工数'"
+					+ "FROM tasks"
+					+ "JOIN man_hours"
+					+ "ON tasks.task_id = man_hours.task_id"
+					+ "GROUP BY tasks.task_id";
+			System.out.println(sql);
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			
+			//セレクト文を実行し結果票を取得
+			ResultSet rs = pStmt.executeQuery();
+			
+			while(rs.next()) {
+				AllDTO dto = new AllDTO();
+				dto.setTaskId(rs.getInt("task_id"));
+				dto.setTaskName(rs.getString("task_name"));
+				dto.setTodayManHours(rs.getDouble("today_man_hours"));
+				list.add(dto);
+			}
+			rs.close();
+			pStmt.close();
+
+			return list;
+			
+		}
 	
 }
--- source/src/main/java/service/ManHourService.java
+++ source/src/main/java/service/ManHourService.java
@@ -1,24 +1,23 @@
 package service;
 
-import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 import dao.ManHourDAO;
 import dto.AllDTO;
 
-public class ManHourService {
+public class ManHourService extends DBAccess{
 	
 	public ManHourService() {
 		super.access();  //DBAccessのaccessを継承
 	}
 	
 	//実績工数
-	public ArrayList<AllDTO> selectManHours(String case_id) {
+	public ArrayList<AllDTO> selectManHours() {
 		ArrayList<AllDTO> list = null;
 		ManHourDAO dao = new ManHourDAO(conn);
 		try {
-			list = dao.selectManHours(case_id);
+			list = dao.selectManHours();
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
@@ -27,7 +26,7 @@ public class ManHourService {
 	}
 	
 	//工数登録メソッド
-	public int registManHour(Double today_man_hours, String work_details, Date work_date){
+	public int registManHour(Double today_man_hours, String work_details, String work_date){
 		ManHourDAO dao = new ManHourDAO(conn);
 		int ans = 0;
 		try {
@@ -35,7 +34,7 @@ public class ManHourService {
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
-		return 0;
+		return ans;
 		
 	}
 	
@@ -48,31 +47,44 @@ public class ManHourService {
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
-		return 0;
+		return ans;
 		
 	}
 	
 	//工数ログ
-	public ArrayList<AllDTO> sumCaseManHours(int task_id){
+	public ArrayList<AllDTO> sumCaseManHours(){
 		ArrayList<AllDTO> list = null;
 		ManHourDAO dao = new ManHourDAO(conn);
 		try {
-			list = dao.sumCaseManHours(task_id);
+			list = dao.sumCaseManHours();
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return list;
 	}
 	
 	//案件名・タスク名（工数登録）
-	public ArrayList<AllDTO> selectCaseName(int task_id){
+	public ArrayList<AllDTO> selectCaseName(){
 		ArrayList<AllDTO> list = null;
 		ManHourDAO dao = new ManHourDAO(conn);
 		try {
-			list = dao.selectCaseName(task_id);
+			list = dao.selectCaseName();
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return list;
+	}
+	
+	//タスクごとの実績工数
+	public ArrayList<AllDTO> sumTaskManHours(){
+		ArrayList<AllDTO> list = null;
+		ManHourDAO dao = new ManHourDAO(conn);
+		try {
+			list = dao.sumCaseManHours();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+		
 	}
 }

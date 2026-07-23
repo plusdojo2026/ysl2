722733bb765bc3edb1977c7b0e9eb04317585dd5
mukogawa-ryo <ryo.mukogawa@ysl.co.jp>
2026-07-23 14:29:30 +0900

    ダッシュボードアクション

--- source/src/main/java/action/DashBoardAction.java
+++ source/src/main/java/action/DashBoardAction.java
@@ -32,19 +32,20 @@ public class DashBoardAction {
 		ArrayList<AllDTO> OvertaskList = service.selectOverTasks(userId);//タスクのデータをTaskListに格納
 		request.setAttribute("OvertaskList",OvertaskList);//taskListの名前でtaskListをセット
 		int overTasks = OvertaskList.size();//該当レコード数を計算 int型の変数"overTasks"にレコード数を格納
+		request.setAttribute("overTasks",overTasks);
 		
 		//担当タスク
 		ArrayList<AllDTO> MytaskList = service.selectAssignedTasks(userId);//タスクのデータをTaskListに格納
 		request.setAttribute("MytaskList",MytaskList);//taskListの名前でtaskListをセット
 		int myTasks = MytaskList.size();//該当レコード数を計算 int型の変数"myTasks"にレコード数を格納
-		
+		request.setAttribute("myTasks",myTasks);
 		
 		//進行中案件
 		DashBoardService caseService = new DashBoardService();//ケースserviceをnewでインスタンス化
 		ArrayList<CaseDTO> caseList = caseService.selectWorkingCases(userId);//案件のデータをCaseListに格納
 		request.setAttribute("caseList",caseList);//caseListの名前でcaseListをセット
 		int caseLists = caseList.size();//該当レコード数を計算 int型の変数"caseList"にレコード数を格納
-		request.setAttribute()
+		request.setAttribute("caseLists",caseLists);
 		
 		return page;
 	} 

7a9167a8a2a252a4caac460efc26ce3fcbb2542e
mukogawa-ryo <ryo.mukogawa@ysl.co.jp>
2026-07-24 17:08:56 +0900

    管理者、月次

--- source/src/main/java/action/ManthlySumAction.java
+++ source/src/main/java/action/ManthlySumAction.java
@@ -1,7 +1,7 @@
 package action;
 
-
-
+import java.io.UnsupportedEncodingException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,22 +19,24 @@ public class ManthlySumAction {
 		this.request=request;
 	}
 
-    public String selectManthlySum(){
+    public String selectManthlySum() throws UnsupportedEncodingException {
         String page = "/WEB-INF/jsp/manthly_sum.jsp";
 
 		//値の取得(年月)
+		request.setCharacterEncoding("UTF-8");
 		
 		
-		//集計案件一覧検索
+		
 		ManthlySumService service = new ManthlySumService();
+		
+		//集計案件一覧検索とその件数カウント
 		ArrayList<AllDTO> TotalCasesAndManHours = service.selectManthlySum();
 		request.setAttribute("TotalCasesAndManHours",TotalCasesAndManHours);
-		
-		
-		
+		int ManthlyCases = TotalCasesAndManHours.size();//今月の稼働案件数を取得
+		request.setAttribute("ManthlyCases", ManthlyCases);
 		
 		//月ごとの実績工数
-		
+		ArrayList<AllDTO> 
 		
 		
 		//担当者名ごとかつ月ごとの工数

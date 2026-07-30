9100352dc1c524b7f45c7ce1cb5f9310f0400a15
fujimori-daiki <daiki.fujimori@ysl.co.jp>
2026-07-30 11:40:21 +0900

    フィルター頑張りました。

--- source/src/main/java/filter/AuthFilter.java
+++ source/src/main/java/filter/AuthFilter.java
@@ -6,10 +6,9 @@ import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import dto.UserDTO;
@@ -18,40 +17,43 @@ import dto.UserDTO;
 public class AuthFilter implements Filter {
 	
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+	public void doFilter(
+			javax.servlet.ServletRequest req, javax.servlet.ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
+		
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
 		
 		System.out.println("ログインチェック");
 		
 		request.setCharacterEncoding("UTF-8");
-
-		String pageId = request.getParameter("pageId");
-		String buttonId = request.getParameter("buttonId");
+		String pageId = request.getParameter("page_id");
+		String buttonId = request.getParameter("button_id");
+		
 		// ログイン処理だけ認証チェック対象外
-		if (pageId==null && buttonId==null) {
-			System.out.println("ログインチェックスキップnull");
+		if (pageId == null && buttonId == null) {
+			System.out.println("ログインチェックスキップ:null");
 			chain.doFilter(request, response);
 			return;
 		}
-		if (pageId.equals("L001") && buttonId.equals("ログイン")) {
+		if ("L001".equals(pageId) && "ログイン".equals(buttonId)) {
 			System.out.println("ログインチェックスキップ:ログイン");
 			chain.doFilter(request, response);
 			return;
 		}
 		
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		
-		//セッションスコープにuser_idが格納されているかのチェック
-		HttpSession session = httpRequest.getSession();
-		
-		//セッションスコープからUserDTOを取り出す
+		// セッションスコープにuser情報が格納されているかのチェック
+		HttpSession session = request.getSession();
 		UserDTO user = (UserDTO) session.getAttribute("user");
 		
-		//userがnullの場合、ログイン画面に戻す
+		// userがnullの場合、ログイン画面に戻す
 		if (user == null) {
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
 			dispatcher.forward(request, response);
 			return;
 		}
+		
+		// ログイン済みなら次の処理へ
+		chain.doFilter(request, response);
 	}
 }
